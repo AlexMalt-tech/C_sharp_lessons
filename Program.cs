b40b7516d5d8ed0aa127d85447f824f6b5165e78
Алексей Мальцев <code.box@bk.ru>
2022-11-30 23:13:11 +0300

    ДЗ 6.1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+void Print(int[] collect)
+{
+    for(int el = 0; el < collect.Length; el++)
+    {
+        System.Console.Write($"{collect[el]}, ");
+    }
+}
+
+int count = 6;
+int[] arr = new int[count];
+int zero = 0;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    System.Console.Write($"Ведите {i} число: ");
+    arr[i] = int.Parse(Console.ReadLine());
+    if (arr[i] > 0)
+        zero++;
+}
+
+Print(arr);
+System.Console.WriteLine($" -> {zero}");
