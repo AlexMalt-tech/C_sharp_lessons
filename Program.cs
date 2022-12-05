6b745956f7eb17260baca8eb79cfa630b205eba3
Алексей Мальцев <code.box@bk.ru>
2022-12-05 20:12:19 +0300

    ДЗ 7.1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
 
-float y = 0, x = 0;
-float b1 = 2, k1 = 5, b2 = 4, k2 = 9;
+static double RandomNumber()
+{
+	Random rnd = new Random();
+	return rnd.Next(-10, 11) + Math.Round((double)rnd.NextDouble(), 1);
+}
 
-x = (b2 - b1) / (k1 - k2);
-y = k1 * x + b1;
+void MakeMatrix(int m, int n)
+{
+	double[,] matrix = new double[m, n];
+	for(int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{	
+			matrix[i, j] = RandomNumber();
+			System.Console.Write($"{matrix[i, j]} ");
+			
+		}
+		System.Console.WriteLine();
+	}
+}
 
-System.Console.WriteLine($"{x}, {y}");
+int rows = 0, columns = 0;
+System.Console.Write("Введите число строк: ");
+rows = int.Parse(Console.ReadLine());
+System.Console.Write("Введите число столбцов: ");
+columns = int.Parse(Console.ReadLine());
+MakeMatrix(rows, columns);
