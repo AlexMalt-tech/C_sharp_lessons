fccece747e3a9a4a85a2e6129ea8d08c97ab24bd
Alex-tech <code.box@bk.ru>
2022-12-08 17:34:31 +0300

    ДЗ 8.2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
-﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 static int RandomNumber() // Генератор случайных чисел
 {
 	Random rnd = new Random();
@@ -38,29 +36,24 @@ void PrintTable(int[,] matrix, int m, int n) //Печать матрицы
 }
 
 int rows = RandomNumber(); // Число строк
-int collumns = RandomNumber(); // Число столбцов
+int collumns = rows; // Число столбцов
 int[,] table = new int[rows, collumns]; // Массив под матрицу
-int[,] tableReverse = new int[rows, collumns]; // Перевернутый массив
+int[] rowSum = new int[rows];
+
 MakeMatrix(table, rows, collumns); // Создание матрицы
 PrintTable(table, rows, collumns);
-System.Console.WriteLine("--------------");
-for(int i = 0; i < rows; i++)
+System.Console.WriteLine("--------------------");
+
+for (int i = 0; i < rows; i++)
 {
-    int[] arr = new int[collumns];
-
-    for(int el = 0; el < collumns; el++)
-        {
-            arr[el] = table[i, el];
-        }
-
-    Array.Sort(arr);
-    Array.Reverse(arr);  
-    for(int el = 0; el < collumns; el++)
-        {
-            tableReverse[i, el] = arr[el];
-        }
+    for (int j = 0; j < collumns; j++)
+    {
+        rowSum[i] += table[i, j];
+    }
 }
-PrintTable(tableReverse, rows, collumns);
+
+System.Console.WriteLine("Строка с наименьшей суммой элементов: " + Array.IndexOf(rowSum, rowSum.Min()));
+
 
 
 
