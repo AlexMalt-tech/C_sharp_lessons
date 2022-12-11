b60f99c35570b3b6b3ef06718ff9aadc268cf806
Alex <67325048+AlexMalt-tech@users.noreply.github.com>
2022-12-11 18:10:39 +0300

    ДЗ 9.2

--- Program.cs
+++ Program.cs
@@ -1,41 +1,13 @@
-﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-// Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
-int[,,] CreateMatrix (int row, int col, int dep, int min, int max) 
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int num1 = 1, num2 = 15;
+
+int SumNum (int num, int stop)
 {
-    int[,,] matrix = new int[row, col, dep];
-    Random rnd = new Random();   
-    for (int i = 0; i < matrix.GetLength(0); i++) 
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            for (int k = 0; k < matrix.GetLength(2); k++)
-                {
-                    matrix[i,j,k] = rnd.Next(min, max + 1);
-                }
-        }
-    }
-    return matrix;
-} 
- 
-void PrintMatrix(int[,,] matrix)
-{
-    for (int i = 0; i <  matrix.GetLength(0); i++)
-    {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++) 
-        {
-            for (int k = 0; k < matrix.GetLength(2); k++)
-            {
-            Console.Write($"{matrix[i, j, k]}({i},{j},{k})|");            
-            }
-        }
-        Console.WriteLine();        
-    }        
+	if (num > stop) return 0;
+	return num + SumNum(num + 1, stop);
 }
- 
-int[,,] array3D = CreateMatrix(2, 2, 2, 10, 99);
-PrintMatrix(array3D);
+
+System.Console.WriteLine(SumNum(num1, num2));
